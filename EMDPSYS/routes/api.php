7c0c2b50d2839fa5042f38a19c06a37ec4e5946b
Daniel Bravo Alvarez Calderon <danielbravo1202@gmail.com>
2020-08-21 18:48:36 -0500

    ultimos-cambios

--- EMDPSYS/routes/api.php
+++ EMDPSYS/routes/api.php
@@ -3,6 +3,35 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Use App\Producto;
+
+Route::get('productos', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Producto::all();
+});
+
+Route::get('productos/{id}', function($id) {
+    return Producto::find($id);
+});
+
+Route::post('productos', function(Request $request) {
+    return Producto::create($request->all);
+});
+
+Route::put('productos/{id}', function(Request $request, $id) {
+    $producto = Producto::findOrFail($id);
+    $producto->update($request->all());
+
+    return $producto;
+});
+
+Route::delete('productos/{id}', function($id) {
+    Producto::find($id)->delete();
+
+    return 204;
+});
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
