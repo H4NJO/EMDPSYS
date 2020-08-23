49604c78916efa863e035d3929c8a4eccdc14a29
Lostickjc <66395292+Lostickjc@users.noreply.github.com>
2020-08-22 20:15:49 -0500

    sdas

---
+++ EMDPSYS/app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','unauthorized']]);
+    	$this->guard = "api";
+    }
+
+    public function unauthorized() 
+    {
+    	return response()->json(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth($this->guard)->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function me()
+    {
+        return response()->json(auth($this->guard)->user());
+    }
+
+    public function logout()
+    {
+        auth($this->guard)->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth($this->guard)->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth($this->guard)->factory()->getTTL() * 60
+        ]);
+    }
+}
